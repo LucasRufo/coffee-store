3a7c0c60e017cb2ac035311c3054b9a29ec77911
Lucas Rufo <rifelu25@outlook.com>
2023-01-09 18:48:24 -0300

    refactor: move swagger config to separate file

---
+++ projects/auth/src/CoffeeStore.Auth.Api/Configuration/SwaggerConfiguration.cs
@@ -0,0 +1,19 @@
+﻿namespace CoffeeStore.Auth.Api.Configuration;
+
+public static class SwaggerConfiguration
+{
+    public static void AddSwagger(this IServiceCollection services)
+    {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+    }
+
+    public static void UseSwagger(this WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+    }
+}
